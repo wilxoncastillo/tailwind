d1b0b954869ca4305150341fde3a68b4c1e8acd8
Wilson Castillo <wilxoncastillo@gmail.com>
2022-01-24 10:48:43 -0500

    13. Dándole forma al logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: [],
+    extend: {
+      colors: {
+        "azul-claro": "#37bcf9",
+        "azul-osucro": "#03370b9"
+      }
+    },
   },
   plugins: [],
 }
